874b7e95f6afcbc1bd59a6de951dccefd9d62eb9
Zgreatch <franktchetta54@gmail.com>
2023-06-24 12:53:02 +0100

    This is my Portfolio Backend

---
+++ config/app.config.js
@@ -0,0 +1,10 @@
+require("dotenv").config;
+
+const MONGO_DB_CONFIG={
+    DB:'mongodb+srv://tchetzzgreat:XZ57lA1jPXgzK61e@myportfoliobackend.as4hvyf.mongodb.net/ReactFrontendProject?retryWrites=true&w=majority'
+}
+
+
+module.exports = {
+    MONGO_DB_CONFIG
+}
